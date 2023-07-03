d52478d565de6da568f7eb58ada4dad38cbeae6c
LAM Frank <frank.lam@soprasteria.com>
2023-07-03 11:27:20 +0100

    feat: Add ESLint and Prettier configurations

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "standard-with-typescript",
+    "plugin:react/recommended",
+    "next",
+    "next/core-web-vitals",
+    "plugin:prettier/recommended",
+    "next/core-web-vitals",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["react", "prettier"],
+  rules: {},
+};
