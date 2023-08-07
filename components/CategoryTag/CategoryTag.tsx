8412655016b1c8000d81746bd496097ae8c30be3
Frank Lam <frankltk55@gmail.com>
2023-08-07 16:19:25 +0100

    style: add category tag active&hover state style

--- components/CategoryTag/CategoryTag.tsx
+++ components/CategoryTag/CategoryTag.tsx
@@ -1,6 +1,8 @@
 import { type ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 
+import useStore from "@/lib/store";
+
 interface CategoryTagProps {
   categoryName: string;
   className?: string;
@@ -10,12 +12,22 @@ export default function CategoryTag({
   categoryName,
   className,
 }: CategoryTagProps): ReactNode {
+  const selectedCategory = useStore((state) => state.selectedCategory);
+
+  const activeTag =
+    selectedCategory.toLowerCase() === categoryName.toLowerCase();
+
   return (
     <div className={twMerge("inline-block", className)}>
-      <div className="flex h-[30px] items-center justify-center rounded-lg bg-violet-50 p-4">
-        <div className="text-[13px] font-semibold text-indigo-600">
-          {categoryName}
-        </div>
+      <div
+        className={twMerge(
+          activeTag
+            ? "bg-indigo-600 text-white"
+            : "bg-violet-50 text-indigo-600",
+          "flex h-[30px] items-center justify-center rounded-lg p-4 hover:bg-indigo-200",
+        )}
+      >
+        <div className="text-[13px] font-semibold ">{categoryName}</div>
       </div>
     </div>
   );
